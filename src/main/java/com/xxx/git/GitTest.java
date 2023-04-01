dffea4f893dd61b10283cf3dd1057b2368886a9f
xxx <1550505969@qq.com>
2023-04-01 23:15:27 +0800

    idea second commit

--- src/main/java/com/xxx/git/GitTest.java
+++ src/main/java/com/xxx/git/GitTest.java
@@ -5,6 +5,7 @@ package com.xxx.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello Git");
+        System.out.println("hello Git1");
+        System.out.println("hello Git2");
     }
 }
